eb6dd72dee55310bdcc8ab0089802fbd6e77d313
blastwind <andrewchen14250@gmail.com>
2022-12-01 14:12:32 -0500

    Formatting #4

--- src/HumanGame.hs
+++ src/HumanGame.hs
@@ -1,43 +1,57 @@
-module HumanGame (startHumanGame) where
-    import Graphics.Vty
-    import qualified Graphics.Vty as V
-    import Control.Monad (void)
-    import Types
-    import UI
-    import Logic
-    import Brick (App (..), BrickEvent (..), Next, EventM, neverShowCursor, attrMap, halt, continue, attrName, fg, customMain)
-    
+module HumanGame
+    ( startHumanGame
+    ) where
+import           Brick                          ( App(..)
+                                                , BrickEvent(..)
+                                                , EventM
+                                                , Next
+                                                , continue
+                                                , customMain
+                                                , halt
+                                                , neverShowCursor
+                                                )
+import           Control.Monad                  ( void )
+import           Graphics.Vty
+import qualified Graphics.Vty                  as V
+import           Logic
+import           Types
+import           UI
 
-    app :: App Game () ()
-    app = App { appDraw = drawUI
-            , appChooseCursor = neverShowCursor
-            , appHandleEvent = handleEvent
-            , appStartEvent = return -- do nothing
-            , appAttrMap = const $ attrMap Graphics.Vty.defAttr [(attrName "ok", fg blue)]
-            }
 
-    handleEvent :: Game -> BrickEvent () () -> EventM () (Next Game)
-    handleEvent g (VtyEvent (V.EvKey V.KEsc [])) = halt g
-    handleEvent g (VtyEvent (V.EvKey V.KDown [])) = continue $ moveHighlight g Types.Down
-    handleEvent g (VtyEvent (V.EvKey V.KUp [])) = continue $ moveHighlight g Types.Up
-    handleEvent g (VtyEvent (V.EvKey V.KLeft [])) = continue $ moveHighlight g Types.Left
-    handleEvent g (VtyEvent (V.EvKey V.KRight [])) = continue $ moveHighlight g Types.Right
+app :: App Game () ()
+app = App { appDraw         = drawUI
+          , appChooseCursor = neverShowCursor
+          , appHandleEvent  = handleEvent
+          , appStartEvent   = return -- do nothing
+          , appAttrMap      = const tictactoeAttrMap
+          }
 
-    handleEvent g _ = continue g
+handleEvent :: Game -> BrickEvent () () -> EventM () (Next Game)
+handleEvent g (VtyEvent (V.EvKey V.KEsc [])) = halt g
+handleEvent g (VtyEvent (V.EvKey V.KDown [])) =
+    continue $ moveHighlight g Types.Down
+handleEvent g (VtyEvent (V.EvKey V.KUp [])) =
+    continue $ moveHighlight g Types.Up
+handleEvent g (VtyEvent (V.EvKey V.KLeft [])) =
+    continue $ moveHighlight g Types.Left
+handleEvent g (VtyEvent (V.EvKey V.KRight [])) =
+    continue $ moveHighlight g Types.Right
 
-    initGame :: IO Game
-    initGame = return $
-        Game {
-            _grid = [[Just O, Just O, Nothing],
-            [Just O, Nothing, Just X],
-            [Just O, Just O, Nothing]],
-            _highlightLocation = (1, 0),
-            _curPlayer = Player1,
-            _done = False
-        }
+handleEvent g _ = continue g
 
-    startHumanGame :: IO ()
-    startHumanGame = do
-        g <- initGame
-        initialVty <- V.mkVty V.defaultConfig
-        void $ customMain initialVty (V.mkVty V.defaultConfig) Nothing app g
+initGame :: IO Game
+initGame = return $ Game
+    { _grid              = [ [Just O, Just O, Nothing]
+                           , [Just O, Nothing, Just X]
+                           , [Just O, Just O, Nothing]
+                           ]
+    , _highlightLocation = (1, 0)
+    , _curPlayer         = Player1
+    , _done              = False
+    }
+
+startHumanGame :: IO ()
+startHumanGame = do
+    g          <- initGame
+    initialVty <- V.mkVty V.defaultConfig
+    void $ customMain initialVty (V.mkVty V.defaultConfig) Nothing app g
--- src/UI.hs
+++ src/UI.hs
@@ -1,22 +1,54 @@
-module UI (module UI) where 
-    import Types
-    import Brick (Widget)
-    import qualified Brick.Widgets.Border.Style as BS
-    import qualified Brick.Widgets.Border as B
-    import Brick.Widgets.Core
-    import Brick.Widgets.Center
-    
-    
-    printTile :: Tile -> String
-    printTile = maybe " " show
+module UI
+    ( module UI
+    ) where
+import           Brick                          ( AttrMap
+                                                , AttrName
+                                                , Widget
+                                                , attrMap
+                                                , attrName
+                                                )
+import qualified Brick                         as U
+import qualified Brick.Widgets.Border          as B
+import qualified Brick.Widgets.Border.Style    as BS
+import           Brick.Widgets.Center
+import           Brick.Widgets.Core
+import qualified Graphics.Vty                  as V
+import           Types
 
-    drawGrid :: Game -> Widget ()
-    drawGrid g = withBorderStyle BS.unicodeBold
+highlightBorderAttr :: AttrName
+highlightBorderAttr = attrName "highlightBorderAttr"
+
+tictactoeAttrMap :: AttrMap
+tictactoeAttrMap = attrMap V.defAttr [(highlightBorderAttr, U.fg V.cyan)]
+
+printTile :: Tile -> String
+printTile = maybe " " show
+
+drawGrid :: Game -> Widget ()
+drawGrid g =
+    withBorderStyle BS.unicodeBold
         $ B.borderWithLabel (str "Tic Tac Toe")
         $ vBox columnWidgets
-        where
-            columnWidgets = [makeRowWidget rowTiles rowInd | (rowTiles, rowInd) <- zip (_grid g) [(0 :: Int)..]]
-            makeRowWidget row rowInd = hLimit 27 $ hBox $ [B.border $ hCenter $ padAll 1 $ str $ (if (rowInd, colInd) == _highlightLocation g then "highlight" else printTile tile) | (tile, colInd) <- zip row [(0 :: Int)..]]
+  where
+    columnWidgets =
+        [ makeRowWidgets rowTiles rowInd
+        | (rowTiles, rowInd) <- zip (_grid g) [(0 :: Int) ..]
+        ]
+    makeRowWidgets rowTiles rowInd =
+        hLimit 27
+            $ hBox
+            $ [ makeCellWidget tile rowInd colInd
+              | (tile, colInd) <- zip rowTiles [(0 :: Int) ..]
+              ]
+    makeCellWidget tile rowInd colInd =
+        B.border
+            $ hCenter
+            $ padAll 1
+            $ str
+            $ (if (rowInd, colInd) == _highlightLocation g
+                  then "highlight"
+                  else printTile tile
+              )
 
-    drawUI :: Game -> [Widget ()]
-    drawUI g = [center $ drawGrid g]
+drawUI :: Game -> [Widget ()]
+drawUI g = [center $ drawGrid g]
